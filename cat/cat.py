d9ae7decbf4bc8221a6638b1641c068b3c3e1af8
Kevin Lin <klin0916@gmail.com>
2024-04-10 23:41:47 -0400

    Comments and thoughs

--- cat/cat.py
+++ cat/cat.py
@@ -5,14 +5,12 @@ import sys
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # logging.disable(logging.CRITICAL)
 
-def catalanNum(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return (4 * n - 2) * catalanNum(n - 1) / (n + 1)
+def expandCatalan(cNums, n):
+    pass
 
 if __name__ == "__main__":
 
+    # Process the RNA string first
     filename = sys.argv[1]
     rnaString = ''
     with open(filename, 'r') as f:
@@ -25,3 +23,17 @@ if __name__ == "__main__":
 
     logging.debug(rnaString)
 
+    # Catalan Numbers
+    # For points {i, j} and {k, l} they are not crossing if i < k < j < l
+    # C_0 = 1 and C_1 = 1 because there is only one way to arrange 0 and 1 node
+    # Complete graph is K_2n
+    # m is to vary over all possible n - 1 choices between 1 and 2n (2n is basically representing all even numbers)
+    # then sum up every possibility of m and return the result
+    # the additional conditions when applied to rna are the following
+    # - Not every even number is can bond with node 1 (A <> U and C <> G)
+    # - Not every slice of (1, m) can match perfectly depending on number of AUCG's in the slice
+    # - The matches in the slice also have to be noncrossing
+    # - This makes each subslice its own noncrossing perfect matching problem
+    # - If any of the above conditions are not met, that m can be immediately discarded
+
+    # So if index-0 is 1 then m has to start from 1,3,5 and so on, so there are an even number of nodes trapped under the first slice
